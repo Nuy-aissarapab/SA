f28f8d420812f42b0ef6344e2eb1b339f35c75c7
coconut <kong15926@gmail.com>
2025-09-01 14:28:01 +0700

    deleate comment

--- frontend/src/interfaces/Announcement..ts
+++ frontend/src/interfaces/Announcement..ts
@@ -7,5 +7,5 @@ export interface AnnouncementInterface {
   UpdatedAt?: string; // ควรเป็นวันที่ในรูปแบบ ISO 8601 (เช่น "2023-10-01T12:00:00Z")
   AdminID?: number; // ผู้ดูแลระบบที่โพสต์ประกาศ
   AnnouncementTargetID?: number; // ประเภทเป้าหมายของประกาศ
-  AnnouncementTypeID?: number; // ประเภทของประกาศ
+  AnnouncementTypeID?: number; 
 }
