2acd07f6ed8d47a3a8617775bff8403717169302
zolasia1123 <guymini02479@gmail.com>
2025-09-03 01:58:47 +0700

    Update billing.go

--- backend/entity/billing.go
+++ backend/entity/billing.go
@@ -7,20 +7,20 @@ import (
 
 type Billing struct {
 	gorm.Model
-	Billing_date	time.Time	`json:"Billing_date"`
-	Invoice			time.Time	`json:"invoice"`
-	Due_Date		time.Time	`json:"due_Date"`
-	Status		 	string		`json:"status"`
+	Billing_date time.Time `json:"Billing_date"`
+	Invoice      time.Time `json:"invoice"`
+	Due_Date     time.Time `json:"due_Date"`
+	Status       *string `json:"status"` // ✅ ต้องเป็น pointer
+	AmountDue    float64  `json:"amount_due" gorm:"type:decimal(10,2)"` // ✅ ยอดที่ต้องชำระ
 
 	// Foreign key → Student
-    StudentID uint
-    Student   Student `gorm:"foreignKey:StudentID" json:"student"`
+	StudentID uint    `json:"student_id"`
+	Student   Student `gorm:"foreignKey:StudentID" json:"student"`
 
 	// 1 Billing มีได้หลาย Payment
-	Payment []Payment `gorm:"foriegnKey:Billing_ID"`
+	Payment []Payment `gorm:"foreignKey:BillingID" json:"payment"`
 
-	// FK ไปที่ Contract
-	ContractID uint
-	Contract   Contract `gorm:"foreignKey:ContractID"`
-
+	// FK → Contract
+	ContractID uint     `json:"contract_id"`
+	Contract   Contract `gorm:"foreignKey:ContractID" json:"contract"`
 }
