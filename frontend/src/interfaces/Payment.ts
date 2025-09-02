e863ae6cb569eec22194b2fe356b9772c35fc623
zolasia1123 <guymini02479@gmail.com>
2025-09-03 02:04:07 +0700

    Update Payment.ts

--- frontend/src/interfaces/Payment.ts
+++ frontend/src/interfaces/Payment.ts
@@ -1,10 +1,25 @@
+// interfaces/Payment.ts
 import type { StudentInterface } from "./Student";
+import type { AdminInterface } from "./Admin";
+import type { BillingInterface } from "./Billing";
 
 export interface PaymentInterface {
   ID?: number;
-  Payment_Date?: string;   // ใช้ string จาก API
+  Payment_Date?: string;
   Amount?: number;
-  Payment_Status?: string;
+  Payment_Status?: "pending" | "remaining" | "paid" | null;
+
+  Method?: string;
+  PayerName?: string;
+  ReceiptNumber?: string;
+  EvidenceURL?: string;
+
+  ReceiverID?: number | null;
+  Receiver?: AdminInterface;
+
+  StudentID?: number;
   Student?: StudentInterface;
 
+  BillingID?: number;
+  Billing?: BillingInterface;
 }
