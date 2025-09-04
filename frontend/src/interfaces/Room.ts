ed7b030a641797ad256349781d0c297386244e7d
Nuy-aissarapab <naissarapab@gmail.com>
2025-09-04 17:04:35 +0700

    Update Room.ts

---
+++ frontend/src/interfaces/Room.ts
@@ -0,0 +1,9 @@
+export interface RoomInterface {
+  ID?: number;
+  RoomNumber?: string;
+  room_number?: string;
+  Room_Status?: string;
+  room_status?: string;
+  Image?: string;
+  image?: string;
+}
