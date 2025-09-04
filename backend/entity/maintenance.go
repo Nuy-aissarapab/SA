39cfe7244f4d249dd32c4673ebfea66005846430
Nuy-aissarapab <naissarapab@gmail.com>
2025-09-04 16:43:08 +0700

    Update maintenance.go

--- backend/entity/maintenance.go
+++ backend/entity/maintenance.go
@@ -1,29 +1,28 @@
 package entity
 
 import (
-	"time"
-	"gorm.io/gorm"
+    "time"
+    "gorm.io/gorm"
 )
 
-type MaintenanceRequest struct {
-	gorm.Model
-	Title         string            `json:"title"`
-	Description   string            `json:"description"`
-	PhotoURL      string            `json:"photo_url"`
-	RequestDate   time.Time         `json:"request_date"`
-	
-	Room_ID *uint
-	Room   Room `gorm:"foriegnKey:Room_ID"`
+type Maintenance struct {
+    gorm.Model
+    Title      string    `json:"title"`
+    Detail     string    `json:"detail"`
+    ReportDate time.Time `json:"report_date"`
 
-	Student_ID *uint
-	Student Student `gorm:"foriegnKey:Student_ID"`
+    ImageURL  *string `json:"image_url"`
+    ImageName *string `json:"image_name"`
 
-	Admin_ID *uint
-	Admin   Admin `gorm:"foriegnKey:Admin_ID"`
+    StudentID *uint   `json:"student_id"`
+    Student   Student `gorm:"foreignKey:StudentID" json:"student"`
 
-	ProblemType_ID *uint
-	ProblemType ProblemType `gorm:"foriegnKey:ProblemType_ID"`	
+    RoomID *uint `json:"room_id"`
+    Room   Room  `gorm:"foreignKey:RoomID" json:"room"`
 
-	MaintenanceStatus_ID *uint
-	MaintenanceStatus MaintenanceStatus `gorm:"foriegnKey:MaintenanceStatus_ID"`
+    ProblemTypeID *uint       `json:"problem_type_id"`
+    ProblemType   ProblemType `gorm:"foreignKey:ProblemTypeID" json:"problem_type"`
+
+    MaintenanceStatusID *uint             `json:"maintenance_status_id"`
+    MaintenanceStatus   MaintenanceStatus `gorm:"foreignKey:MaintenanceStatusID" json:"maintenance_status"`
 }
