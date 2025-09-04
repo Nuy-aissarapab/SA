bfdcd2216b73404a958d57d4d1333cef18f95e8c
Nuy-aissarapab <naissarapab@gmail.com>
2025-09-04 16:46:55 +0700

    Add files via upload

---
+++ backend/controller/problemtype/problemtype.go
@@ -0,0 +1,17 @@
+package problemtype
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/SA/config"
+	"github.com/SA/entity"
+)
+
+func List(c *gin.Context) {
+	var items []entity.ProblemType
+	if err := config.DB().Find(&items).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, items)
+}
