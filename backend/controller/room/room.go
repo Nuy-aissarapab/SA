b0dc62b78f47b3c87857da21fa664965da55b1eb
Nuy-aissarapab <naissarapab@gmail.com>
2025-09-04 17:00:45 +0700

    Add files via upload

---
+++ backend/controller/room/room.go
@@ -0,0 +1,21 @@
+package room
+
+import (
+	"net/http"
+	"github.com/SA/config"
+	"github.com/gin-gonic/gin"
+	"github.com/SA/entity"
+)
+
+func GetAll(c *gin.Context) {
+	var rooms []entity.Room
+
+	if err := config.DB().Raw("SELECT * FROM rooms").Scan(&rooms).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": rooms})
+
+}
+
