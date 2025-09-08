c0423cd82195840ada2d339799c44fed3653ebe8
zolasia1123 <guymini02479@gmail.com>
2025-09-08 13:54:11 +0700

    Update contract.go

--- backend/entity/contract.go
+++ backend/entity/contract.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"time"
+
 	"gorm.io/gorm"
 )
 
@@ -11,19 +12,6 @@ type Contract struct {
 	Start_Date time.Time `json:"start_date"`
 	End_Date   time.Time `json:"end_date"`
 	Rate       float64   `json:"rate" gorm:"type:decimal(10,2)"`
-  
-	Billings   []Billing `gorm:"foreignKey:ContractID"`
-  
-	Room_ID   *uint
-	Room      Room   `gorm:"foreignKey:Room_ID"`
-
-	Admin_ID  *uint
-	Admin     Admin  `gorm:"foreignKey:Admin_ID"`
-	
-	StudentID *uint
-	Student   Student `gorm:"foreignKey:StudentID"`
-  
-	// ใน entity.Contract
 	RenewalPending   bool       `json:"renewal_pending" gorm:"default:false"`
 	RenewalMonths    *int       `json:"renewal_months"`
 	RenewalStartDate *time.Time `json:"renewal_start_date"`
@@ -31,5 +19,17 @@ type Contract struct {
 	RenewalRate      *float64   `json:"renewal_rate"`
 	RenewalStatus    *string    `json:"renewal_status"` // "pending" | "approved" | "rejected"
 
-  }
-  
+	RoomID *uint `json:"Room_ID"`
+	Room    Room    // ความสัมพันธ์ย้อนกลับ
+
+	Admin_ID *uint
+	Admin    Admin `gorm:"foreignKey:Admin_ID"`
+
+	StudentID *uint
+	Student   *Student `gorm:"foreignKey:StudentID"`
+	
+	PaymentID *uint
+	Payment   *Payment `gorm:"foreignKey:PaymentID"`
+
+
+}
